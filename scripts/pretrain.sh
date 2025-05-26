89324b8db45fa482d7584945749cf36f6dfa63e0
YangYining <YangYN@sjtu.edu.cn>
2025-05-26 11:46:11 -0500

    updated command

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -87,6 +87,7 @@ deepspeed pannot/train/train_mem.py \
     --mm_seq_projector_type linear \
     --mm_seq_select_layer -1 \
     --mm_seq_select_feature "cls"\
+    --mm_seq_no_pooling True \
     --use_str_tower True \
     --mm_struc_tower $STR_TOWER \
     --mm_str_projector_type linear \
