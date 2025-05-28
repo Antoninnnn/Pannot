d3d5bc522c60395854cf5d87a176d12bf44e1fe9
YangYining <YangYN@sjtu.edu.cn>
2025-05-27 19:43:08 -0500

    add wandb setup in code

--- pannot/train/train_mem.py
+++ pannot/train/train_mem.py
@@ -4,5 +4,9 @@ import os
 # Set shared cache directory across all nodes
 os.environ["TORCH_HOME"] = os.environ.get("TORCH_HOME", "/scratch/user/yining_yang/.cache/torch")
 
+os.environ["WANDB_MODE"] = "offline"
+os.environ["WANDB_DIR"] = os.environ.get("SCRATCH", "/tmp") + "/wandb_logs"
+# Optional: keep if you ever switch to online
+os.environ["WANDB_API_KEY"] = "c6da89ba565a8b25f5b18c6fb722e7ad6637d4de"
 if __name__ == "__main__":
     train(attn_implementation="flash_attention_2")
